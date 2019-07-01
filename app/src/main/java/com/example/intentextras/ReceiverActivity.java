ea90b4cba094984cedf638efc5aa55836ce2acb5
watiri98 <watiricate16@gmail.com>
2019-07-01 22:33:52 +0300

    my intentextras code

---
+++ app/src/main/java/com/example/intentextras/ReceiverActivity.java
@@ -0,0 +1,41 @@
+package com.example.intentextras;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReceiverActivity extends AppCompatActivity {
+    TextView tvName;
+    TextView tvAge;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tvName=findViewById(R.id.tvName);
+        tvAge=findViewById(R.id.tvAge);
+        String savedExtra = getIntent().getStringExtra("value1");
+        tvName.setText(savedExtra);
+        int savedExtraNumber = getIntent().getIntExtra("value2",89);
+        tvAge.setText(savedExtraNumber);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
